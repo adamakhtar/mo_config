465d8141cc17af4ee5654067264ac33b9a96b508
Adam Akhtar <adamsubscribe@googlemail.com>
2023-07-10 23:18:41 +0900

    change: move DSL logic into MoConfig module

--- lib/mo_config.rb
+++ lib/mo_config.rb
@@ -22,7 +22,6 @@ require "mo_config/source/credentials"
 require "mo_config/source/env"
 require "mo_config/setting"
 require "mo_config/dsl"
-require "mo_config/dsl/source_config"
 
 module MoConfig
   class Error < StandardError; end
@@ -32,7 +31,62 @@ module MoConfig
 
   def self.included(base)
     base.class_eval do
-      include Dsl
+      extend ClassMethods
+    end
+  end
+
+  module ClassMethods
+    def source(name, options={}, &settings_block)
+      sources[name] ||= MoConfig::Source.for(name, options)
+
+      settings_config = Dsl.compile_source_and_settings(name, options, &settings_block)
+
+      settings_for_source = settings_config.each_with_object({}) do |setting_config, settings_hash|
+        type_klass = MoConfig::Type.for(setting_config[:type])
+        settings_attrs = setting_config.merge(
+          config_name: self.name,
+          source: sources[name],
+          type: type_klass
+        )
+
+        setting = MoConfig::Setting.new(**settings_attrs)
+        settings_hash[setting.name] = setting
+        settings_hash
+      end
+
+      settings_for_source.each_pair do |setting_name, setting|
+        define_singleton_method setting.name.to_s do
+          setting.value
+        end
+      end
+
+      settings.merge!(settings_for_source)
+    end
+
+    def valid?
+      settings.each_pair do |setting_name, setting|
+        setting.valid?
+
+        setting.errors.each_pair do |error_type, error_messages|
+          errors.add(setting.name, type: error_type, error_messages: error_messages)
+        end
+
+        errors.any?
+      end
+    end
+
+    def errors
+      @errors ||= Errors.new
+    end
+
+    private
+
+    def sources
+      @sources ||= {}
+    end
+
+    def settings
+      @settings ||= {}
     end
   end
 end
--- lib/mo_config/dsl.rb
+++ lib/mo_config/dsl.rb
@@ -1,65 +1,25 @@
 module MoConfig
-  module Dsl
-    def self.included(base)
-      base.class_eval do
-        extend ClassMethods
-      end
+  class Dsl
+    attr_reader :settings_config, :name, :options
+
+    def self.compile_source_and_settings(source_name, source_options, &settings_block)
+      dsl = new(source_name, source_options)
+      dsl.compile_settings(&settings_block)
+      dsl.settings_config
     end
 
-    module ClassMethods
-      def source(name, options={}, &settings_block)
-        source_config = SourceConfig.new(name, options)
-        source_config.compile_settings(&settings_block)
+    def initialize(name, options={})
+      @name = name
+      @options = options
+      @settings_config = []
+    end
 
-        sources[name] ||= MoConfig::Source.for(name, options)
+    def compile_settings(&settings_block)
+      instance_eval(&settings_block)
+    end
 
-        settings_for_source = source_config.settings_config.each_with_object({}) do |setting_config, settings_hash|
-          type_klass = MoConfig::Type.for(setting_config[:type])
-          settings_attrs = setting_config.merge(
-            config_name: self.name,
-            source: sources[name],
-            type: type_klass
-          )
-
-          setting = MoConfig::Setting.new(**settings_attrs)
-          settings_hash[setting.name] = setting
-          settings_hash
-        end
-
-        settings_for_source.each_pair do |setting_name, setting|
-          define_singleton_method setting.name.to_s do
-            setting.value
-          end
-        end
-
-        settings.merge!(settings_for_source)
-      end
-
-      def valid?
-        settings.each_pair do |setting_name, setting|
-          setting.valid?
-
-          setting.errors.each_pair do |error_type, error_messages|
-            errors.add(setting.name, type: error_type, error_messages: error_messages)
-          end
-
-          errors.any?
-        end
-      end
-
-      def errors
-        @errors ||= Errors.new
-      end
-
-      private
-
-      def settings
-        @settings ||= {}
-      end
-
-      def sources
-        @sources ||= {}
-      end
+    def setting(name, options={})
+      @settings_config << options.merge({name: name})
     end
   end
 end
