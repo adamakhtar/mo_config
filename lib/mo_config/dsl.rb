40de67deea9e429e4ba5d4e34b28adbb015fbee5
Adam Akhtar <adamsubscribe@googlemail.com>
2023-07-10 23:21:42 +0900

    change: rename method

--- lib/mo_config/dsl.rb
+++ lib/mo_config/dsl.rb
@@ -2,7 +2,7 @@ module MoConfig
   class Dsl
     attr_reader :settings_config
 
-    def self.compile_source_and_settings(&settings_block)
+    def self.compile_source_settings(&settings_block)
       dsl = new()
       dsl.compile_settings(&settings_block)
       dsl.settings_config
